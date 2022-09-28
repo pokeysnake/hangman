3e842aad426a735f647351b729d9c52b666b74d8
CameronIFrazier <taterfrazier1@gmail.com>
2022-09-28 13:38:23 -0700

    //test

---
+++ SwingProjectv1point1.zip/SwingProjectv1point1/src/main/java/com/mycompany/swingprojectv1point1/SwingProjectv1point1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.swingprojectv1point1;
+
+/**
+ *
+ * @author tater
+ */
+public class SwingProjectv1point1 {
+
+    public static void main(String[] args) {
+        GameScreen game = new GameScreen();
+       
+        game.setVisible(true);
+       
+    }
+}
